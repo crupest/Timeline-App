bb524c8992fd2b0dcc9eb1f2883831a8682156da
crupest <crupest@outlook.com>
2019-04-13 11:42:26 +0800

    Init

---
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+import { TodoItem } from '../todo-item';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css', '../todo-list-color-block.css']
+})
+export class TodoItemComponent {
+  @Input() item!: TodoItem;
+}
