0c9644bb499723ceaa5228c1f668b9e3908f6947
crupest <crupest@outlook.com>
2019-06-05 22:32:11 +0800

    ...

---
+++ src/app/cru/button/button.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CruButtonDirective } from './button.directive';
+
+describe('ButtonDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CruButtonDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/cru/cru.module.ts
+++ src/app/cru/cru.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CruButtonDirective } from './button/button.directive';
 import { CruLoadingComponent } from './loading/loading.component';
 import { CruMatIconComponent } from './mat-icon/mat-icon.component';
 import { CruIconButtonComponent } from './icon-button/icon-button.component';
 
 @NgModule({
-  declarations: [CruLoadingComponent, CruMatIconComponent, CruIconButtonComponent],
+  declarations: [CruLoadingComponent, CruMatIconComponent, CruIconButtonComponent, CruButtonDirective],
   imports: [CommonModule],
-  exports: [CruLoadingComponent, CruMatIconComponent, CruIconButtonComponent]
+  exports: [CruLoadingComponent, CruMatIconComponent, CruIconButtonComponent, CruButtonDirective]
 })
 export class CruModule {}
